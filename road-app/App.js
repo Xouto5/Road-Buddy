030bb93063f017411dfe0206c9ea21067a293cf9
CoddingTimmy138 <257894520+CoddingTimmy138@users.noreply.github.com>
2026-02-18 17:06:49 -0800

    Login and Profile

--- road-app/App.js
+++ road-app/App.js
@@ -1,12 +1,27 @@
+// Manuel
+
+//import navigation, StatusBar for handling status bar and screens
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import LoginScreen from "./src/features/auth/screens/LoginScreen";
+import ProfileSetupScreen from "./src/features/auth/screens/ProfileSetupScreen";
+
+// Create a stack navigator instance
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <>
-      <LoginScreen />
+    // Wrap the app in NavigationContainer to enable navigation for login, setting profile and
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </>
+    </NavigationContainer>
   );
 }
+
