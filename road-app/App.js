43cf08d0c04880bde7e52beeb58b5c99847f4a8b
Bryan Cardeno <108753983+BryanCardeno@users.noreply.github.com>
2026-02-19 09:57:11 -0800

    minor changes

--- road-app/App.js
+++ road-app/App.js
@@ -5,19 +5,23 @@ import SettingsScreen from "./src/features/settings/screens/SettingsScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/features/trip/screens/HomeScreen";
-
-import TempMenuScreen from "./src/navigation/TempMenu";
 import TripsSummaryScreen from "./src/features/trip/screens/TripsSummaryScreen";
 import TripDetailsScreen from "./src/features/trip/screens/TripDetailsScreen";
 
+// Temporary Menu Screen. Will be removed
+import TempMenuScreen from "./src/navigation/TempMenu";
+
+// Create a stack navigator instance
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    // Added temporary navigation to each screens. Added Manuel's changes.
     <NavigationContainer>
       <Stack.Navigator initialRouteName="TempMenu">
         <Stack.Screen name="TempMenu" component={TempMenuScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
         <Stack.Screen name="Plan" component={HomeScreen} />
         <Stack.Screen name="Overview" component={TripDetailsScreen} />
